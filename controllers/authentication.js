051f6ec278a42b219e8355ebdefdabb431410e0c
Paul WB <higgsbison@gmail.com>
2017-01-24 21:40:05 -0800

    get email and password from request and check if user exists

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,3 +1,16 @@
+const User = require('../models/user');
+
 exports.signup = function(req, res, next) {
-  res.send({ success: 'true' });
+  // see if a user with given email exists
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // if user email already exists, return error
+  User.findOne({ email: email }, function(err, existingUser) {
+     
+  });
+
+  // if user with email does not exist. create and save user record.
+
+  // respond to request indicating the user was created
 }
