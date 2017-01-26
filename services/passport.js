328742f73d0fc4ffaa544aa07baee5b2b03d86b2
Paul WB <higgsbison@gmail.com>
2017-01-26 15:24:59 -0800

    add passport local, check for err, and no matching user

--- services/passport.js
+++ services/passport.js
@@ -3,6 +3,20 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+// create local Strategy
+const localOptions = { usernameField: 'email' };
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+  // verify username and password, call done if it is correct
+  // otherwise call done with false
+  User.findOne({ email: email }, function(err, user) {
+    if (err) { return done(err); }
+    if (!user) { return done(null, false); }
+    // compare passwords
+  });
+
+});
 
 // set up options for jwt strategy
 const jwtOptions = {
