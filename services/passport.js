935402aa1c953f8c601e2a6419cdbb8f5fc96dc9
Paul WB <higgsbison@gmail.com>
2017-01-26 14:17:14 -0800

    add jwt strategy for signing in

--- services/passport.js
+++ services/passport.js
@@ -3,3 +3,24 @@ const User = require('../models/user');
 const config = require('../config');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// set up options for jwt strategy
+const jwtOptions = {}
+
+//create jwt Strategy
+const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
+  // see if user id in the payload exists in the database, if it
+  // does, call done with that user
+  // otherwise call done without a user object
+  User.findById(payload.sub, function(err, user) {
+    if (err) { done(err, false); }
+    if (user) {
+      done(null, user);
+    } else {
+      done(null, false);
+    }
+  });
+
+});
+
+//tell passport to use this strategy
