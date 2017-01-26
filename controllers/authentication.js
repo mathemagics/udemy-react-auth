facdbe282b576a473cb8d7b6fc073d51c30bc2bf
Paul WB <higgsbison@gmail.com>
2017-01-25 19:00:00 -0800

    add save user to db

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -1,16 +1,29 @@
 const User = require('../models/user');
 
 exports.signup = function(req, res, next) {
-  // see if a user with given email exists
+  console.log(req);
   const email = req.body.email;
   const password = req.body.password;
-
-  // if user email already exists, return error
+  console.log(email, password);
+  // see if a user with given email exists
   User.findOne({ email: email }, function(err, existingUser) {
-     
-  });
+     if (err) { return next(err); }
+     console.log(existingUser);
+     // if user email already exists, return error
+    if (existingUser) {
+      return res.status(422).send({ error: 'Email in use' });
+    }
+    // if user with email does not exist. create and save user record.
+    const user = new User({
+      email: email,
+      password: password
+    });
 
-  // if user with email does not exist. create and save user record.
+    user.save(function(err) {
+      if (err) { return next(err); }
+      res.json({ success: true });
+    });
+  });
 
   // respond to request indicating the user was created
 }
