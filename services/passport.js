53d1ec66492d797966ea60773a714bb6eee899d7
Paul WB <higgsbison@gmail.com>
2017-01-26 15:03:52 -0800

    add sign in with jwt

--- services/passport.js
+++ services/passport.js
@@ -6,7 +6,7 @@ const ExtractJwt = require('passport-jwt').ExtractJwt;
 
 // set up options for jwt strategy
 const jwtOptions = {
-  jwfFromRequest: ExtractJwt.fromHeader('authorization'),
+  jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: config.secret
 }
 
@@ -16,14 +16,13 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
   // does, call done with that user
   // otherwise call done without a user object
   User.findById(payload.sub, function(err, user) {
-    if (err) { done(err, false); }
+    if (err) { return done(err, false); }
     if (user) {
       done(null, user);
     } else {
       done(null, false);
     }
   });
-
 });
 
 //tell passport to use this strategy
